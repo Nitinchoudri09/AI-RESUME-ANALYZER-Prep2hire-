c6750d487bdaa7ba1ffbcea6233abe08b21dcd9d
nitin1-sirpi <nitin.sirpi@gmail.com>
2026-05-05 18:39:42 +0530

    Fix deployment structure and start command

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+# Go into the project directory
+cd Prep2Hire
+
+python manage.py collectstatic --no-input
+python manage.py migrate
